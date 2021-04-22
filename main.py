5f4b9670b54c24061bd133dfadcf7672bc991fb5
Hien Pham <phaminhien@hotmail.com>
2021-04-22 17:33:44 +0700

    flip card

--- main.py
+++ main.py
@@ -6,24 +6,37 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pd.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient='records')
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text='French')
-    canvas.itemconfig(card_word, text=current_card['French'])
+    canvas.itemconfig(card_title, text='French', fill='black')
+    canvas.itemconfig(card_word, text=current_card['French'], fill='black')
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text='English', fill='white')
+    canvas.itemconfig(card_word, text=current_card['English'], fill='white')
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
 
 card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
 cross_image = PhotoImage(file="images/wrong.png")
 check_image = PhotoImage(file="images/right.png")
 
 canvas = Canvas(width=800, height=526)
-canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
